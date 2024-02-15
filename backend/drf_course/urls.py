a36a813027cd0e787c8ecedeed96c34339b2083b
Yannick Ferenczi <yannick.ferenczi@outlook.com>
2024-02-15 18:47:04 +0100

    Add content to the core app

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 
+from core import views as core_views
+
+
 router = routers.DefaultRouter()
 
 urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
+    path('contact/', core_views.ContactAPIView.as_view()),
 ]
