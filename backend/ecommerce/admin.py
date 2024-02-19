0d69df633dd105409ab54befe21d6be9e0c098f1
Yannick Ferenczi <yannick.ferenczi@outlook.com>
2024-02-19 15:44:59 +0100

    Add serializers to the ecommerce app

--- backend/ecommerce/admin.py
+++ backend/ecommerce/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item')
