6c6b3bbf8a5daaa674011d6e4df7f6441dafab74
Yannick Ferenczi <yannick.ferenczi@outlook.com>
2024-02-19 15:04:12 +0100

    Add signals for authentication

--- backend/drf_course/settings.py
+++ backend/drf_course/settings.py
@@ -28,7 +28,9 @@ INSTALLED_APPS = [
     'django_extensions', # Great package to access abstract models
     'django_filters', # Used with DRF
     'rest_framework', # DRF package
+    'rest_framework.authtoken', # Used to enable token authentication
     'core',
+    'ecommerce',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework_json_api.parsers.JSONParser',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework_json_api.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer'
--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -1,21 +1,7 @@
-"""drf_course URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 from core import views as core_views
 
@@ -27,4 +13,8 @@ urlpatterns = router.urls
 urlpatterns += [
     path('admin/', admin.site.urls),
     path('contact/', core_views.ContactAPIView.as_view()),
+    path(
+        'api-token-auth/',
+        obtain_auth_token
+    ), # gives us access to token auth
 ]
