474fdef591fb8e12475fca3d56012861ea3bcf22
Yannick Ferenczi <yannick.ferenczi@outlook.com>
2024-02-19 16:09:23 +0100

    Add views and urls to the ecommerce app

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -4,9 +4,12 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
 from core import views as core_views
+from ecommerce import views as ecommerce_views
 
 
 router = routers.DefaultRouter()
+router.register(r'item', ecommerce_views.ItemViewSet, basename='item') # Those are regex urls
+router.register(r'order', ecommerce_views.OrderViewSet, basename='order') # Those are regex urls
 
 urlpatterns = router.urls
 
